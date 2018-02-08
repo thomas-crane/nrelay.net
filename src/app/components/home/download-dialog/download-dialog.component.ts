51fba587e7c81b1272b2b8e4a8f48321b48b29c6
thomas-crane <thomasgcrane@gmail.com>
2018-02-09 10:47:01 +1100

    Add home component with download dialog

---
+++ src/app/components/home/download-dialog/download-dialog.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Inject, HostListener, Output, EventEmitter, Input } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material';
+
+@Component({
+  selector: 'nr-download-dialog',
+  templateUrl: './download-dialog.component.html',
+  styleUrls: ['./download-dialog.component.scss']
+})
+export class DownloadDialogComponent {
+  constructor() { }
+
+  @Input()
+  text: string;
+
+  @Input()
+  maxWidth: number;
+
+  @Output()
+  shouldClose: EventEmitter<any> = new EventEmitter();
+
+  @HostListener('window:mousedown', ['$event'])
+  onMouseDown(event: MouseEvent) {
+    // this.shouldClose.emit();
+  }
+
+  @HostListener('mousedown', ['$event'])
+  onClientMouseDown(event: MouseEvent) {
+    // event.stopPropagation();
+    return true;
+  }
+
+  close(): void {
+    this.shouldClose.emit();
+  }
+}
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
+import { DownloadDialogComponent } from './download-dialog/download-dialog.component';
+import { fadeUp, growDown } from '../../../animations';
+import { GithubService } from '../../services/github.service';
+import { Subject } from 'rxjs/Subject';
+import { takeUntil } from 'rxjs/operators/takeUntil';
+
+@Component({
+  selector: 'nr-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  animations: [fadeUp, growDown]
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  showDialog = false;
+  dialogText: string;
+  dialogWidth = 500;
+  masterVersion: string;
+  devVersion: string;
+  isDestroyed: Subject<any> = new Subject();
+
+  constructor(private githubService: GithubService) {}
+
+  ngOnInit() {
+    this.githubService.getPackageJSON(false).pipe(takeUntil(this.isDestroyed)).subscribe((res) => {
+      this.masterVersion = res.version;
+    });
+    this.githubService.getPackageJSON(true).pipe(takeUntil(this.isDestroyed)).subscribe((res) => {
+      this.devVersion = res.version;
+    });
+  }
+
+  ngOnDestroy() {
+    this.isDestroyed.next();
+    this.isDestroyed.complete();
+  }
+
+  openDialog(stable: boolean) {
+    if (stable) {
+      this.dialogWidth = 500;
+      this.dialogText = 'git clone https://github.com/thomas-crane/nrelay.git';
+    } else {
+      this.dialogWidth = 600;
+      this.dialogText = 'git clone -b dev git clone https://github.com/thomas-crane/nrelay.git';
+    }
+    this.showDialog = true;
+  }
+}
+
