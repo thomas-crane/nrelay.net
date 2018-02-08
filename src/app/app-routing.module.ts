3b53e65036dd378745abf925719cfd0191c34c49
thomas-crane <thomasgcrane@gmail.com>
2018-02-09 10:48:05 +1100

    Update routing and app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DocsComponent } from './components/docs/docs.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'docs/:path',
+    component: DocsComponent
+  },
+  {
+    path: 'docs',
+    component: DocsComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MaterialImportsModule } from './material-imports/material-imports.module';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './components/home/home.component';
+import { DocsComponent } from './components/docs/docs.component';
+import { GithubService } from './services/github.service';
+import { DownloadDialogComponent } from './components/home/download-dialog/download-dialog.component';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { MdRendererDirective } from './directives/md-renderer.directive';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavigationComponent,
+    HomeComponent,
+    DocsComponent,
+    DownloadDialogComponent,
+    MdRendererDirective,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialImportsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [GithubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
