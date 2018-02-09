8054841ce572356d204f9a15a02e0ac572789a7f
thomas-crane <thomasgcrane@gmail.com>
2018-02-09 15:05:55 +1100

    Make sidenav responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'nr-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'nr';
+export class AppComponent implements OnInit {
+  sideMode = 'side';
+  opened = true;
+
+  @HostListener('window:resize', ['$event'])
+  windowResize(event: any) {
+    if (window.innerWidth < 600) {
+      if (this.sideMode !== 'over') {
+        this.sideMode = 'over';
+      }
+      if (this.opened) {
+        this.opened = false;
+      }
+    } else {
+      if (this.sideMode !== 'side') {
+        this.sideMode = 'side';
+      }
+      if (!this.opened) {
+        this.opened = true;
+      }
+    }
+  }
+
+  ngOnInit() {
+    if (window.innerWidth < 600) {
+      this.sideMode = 'over';
+      this.opened = false;
+    }
+  }
+
+  openDrawer(): void {
+    this.opened = true;
+  }
 }
