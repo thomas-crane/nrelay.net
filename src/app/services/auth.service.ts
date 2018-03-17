25c540270cf6e2dab06e5f4b99123314f1ae972a
thomas-crane <thomasgcrane@gmail.com>
2018-03-17 22:38:35 +1100

    Add auth service and auth guard

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { User} from 'firebase';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators/map';
+
+@Injectable()
+export class AuthService {
+
+  public user: User;
+
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe((user: firebase.User) => {
+      this.user = user;
+    }, (err) => {
+      this.user = null;
+    });
+  }
+
+  login(email: string, password: string): Promise<User | null> {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout(): Promise<any> {
+    return this.afAuth.auth.signOut();
+  }
+}
